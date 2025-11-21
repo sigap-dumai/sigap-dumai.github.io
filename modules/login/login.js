49484e3a15f64b946aa29f6cf9c66d1512970f99
dumaikota <ayrakucomel@gmail.com>
2025-11-22 00:58:59 +0700

    sssss

--- modules/login/login.js
+++ modules/login/login.js
@@ -9,14 +9,20 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     .then(response => response.json())
     .then(users => {
       const user = users.find(user => user.username === username && user.password === password);
-      
+
       if (user) {
         // Simpan nama user dan role ke localStorage
         localStorage.setItem('loggedInUser', user.username);
         localStorage.setItem('userRole', user.role);  // Menyimpan role user
 
-        // Redirect ke halaman dashboard
-        window.location.href = '/modules/dashboard/dashboard.html'; // Ganti dengan path dashboard yang sesuai
+        // Cek peran pengguna dan arahkan ke halaman yang sesuai
+        if (user.role === 'admin_kecamatan' || user.role.includes('admin')) {
+          // Jika pengguna adalah admin, arahkan ke admin-reports
+          window.location.href = '/modules/admin-reports/admin-reports.html';
+        } else {
+          // Jika pengguna biasa, arahkan ke report
+          window.location.href = '/modules/report/report.html';
+        }
       } else {
         // Tampilkan pesan error jika login gagal
         alert('Username atau password salah!');
