41ac5fa326e4514a1e9390417af68b5509d98187
sigap-dumai <ayrakucomel@gmail.com>
2026-01-26 06:47:46 +0700

    fix: Update Google Apps Script Web App URL (Versi 3 - 26 Jan 2026)
    NEW URL (Versi 3 - 26 Jan 2026, pukul 06.44):
    AKfycbxyKy7p_OsycQOH_agtoah_E6dNNHSNQEdhERRfW8UPIHoVXgWoGc5HnqOvRUrPrqNQcQ
    
    Mengganti URL deployment sebelumnya yang salah dengan URL deployment yang benar.
    apiBaseUrl sudah diupdate ke deployment Web App yang aktif.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const CONFIG = {
     version: "1.2",
     defaultLat: 1.6815,
     defaultLng: 101.4475,
-    apiBaseUrl: "https://script.google.com/macros/s/AKfycbwFu6MHpPkk5WasJTAlQpP8P9ywv8-r6wRqdtQ602HIgojaGa1HJn0zRvFzZ2GbGUWh/exec",
+    apiBaseUrl: "https://script.google.com/macros/s/AKfycbxyKy7p_OsycQOH_agtoah_E6dNNHSNQEdhERRfW8UPIHoVXgWoGc5HnqOvRUrPrqNQcQ/exec",
     publicApiKey: "SK_TEST_SIGAP_DUMAI_20260126"
 };
