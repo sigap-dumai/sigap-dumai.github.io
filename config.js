5d95be80e8a544d5de4937939eef18fb51cf87df
sigap-dumai <ayrakucomel@gmail.com>
2026-01-26 13:04:18 +0700

    Fix: Update apiBaseUrl ke deployment Versi 3 (pukul 06.44)

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const CONFIG = {
     version: "1.2",
     defaultLat: 1.6815,
     defaultLng: 101.4475,
-    apiBaseUrl: "https://script.google.com/macros/s/AKfycbzUo366gKfnn91jCNtbvClc3ALDg9J3GZkoB_w-x-goSVZufpDhYOcxE0nSgif07E8ssg/exec",
+    apiBaseUrl: "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiDZV1k6Ql7702yXTKUb91rTiy9LBYegObBSPi0eGnEuUVD63XpFzWHtlrDrPj6lFGo_A_0GYMPQkjXWGItKE4nchs9VC9NqbVNu6527LtKXNOPc1uU-tVZtywgJvC1AJMFz0v-38a65IWNUWUse7tTyImhZ1Cc83THtEEKgFzEDuoqJOHCO94eVvSLR8movX34yXE0YsDZFVUWIhZTvvbZ3eD-QBFmGN52LbAEHmX3qWSxXHxvG4auttvLS0WFK4uTdzrK9lAhp5vtFyNDH8v3vqbfgg1nLm7Bzr0Z&lib=MUSmLqSs-T-qa67Q89mzEFQmhA4wTl09FYOcxE0nSgif07E8ssg/exec",
     publicApiKey: "SK_TEST_SIGAP_DUMAI_20260126"
 };
